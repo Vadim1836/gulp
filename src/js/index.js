0f74b00a2c66f9533689919d1ee7197ce12a6909
Vadim1836 <39608037+Vadim1836@users.noreply.github.com>
2023-11-06 19:41:12 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+
+import mobileNav from './modules/mobile-nav.js';
+
+new AirDatepicker('#date');
+
+mobileNav();
+
